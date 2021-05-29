bb619f92e44e01bbc495b943330e656f7402d685
Nathaniel Dao <n.dao.335@westcliff.edu>
2021-05-29 14:25:29 -0700

    W4-lab-sort method
    Sort option 1: normal - sort by ID asc
    Sort option 2: lowest - sort by price asc
    Sort option 3: highest - sort by price desc

---
+++ Module 5/Lab Assignment/m5-w4-lab-assignment/shopcart/src/Home.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from "react";
+import Shopcontent from "./displayProducts";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Home(props){
+  // const shopitems = props;
+  return (
+    <div className="Home">
+      <Shopcontent 
+        shopitems={props.products}
+        handleIncrease={props.handleIncrease} 
+        handleDecrease={props.handleDecrease}
+        sortType={props.sortType}
+        listNum={props.listNum}
+        onSort={props.onSort}
+      />
+    </div>
+  );
+}
